6ca6e6c41afe9fec74213b428be961489d53a582
khatami.maysam@gmail.com <khatami.maysam@gmail.com>
2023-07-02 14:21:07 +0330

    Aryashimi Added Yekanbakh Font

--- wp-content/themes/hdm/header.php
+++ wp-content/themes/hdm/header.php
@@ -27,6 +27,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="<?=get_template_directory_uri()?>/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?=get_template_directory_uri()?>/assets/css/owl.carousel.css">
+    <link rel="stylesheet" href="<?=get_template_directory_uri()?>/assets/fonts/Yekanbakh//YekanBakhs-font.css">
     <link rel="stylesheet" href="<?=get_template_directory_uri()?>/assets/css/style.css">
 
 </head>
