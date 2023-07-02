f81fee3aeb05de9d02f85eb651a3288b3101ac17
khatami.maysam@gmail.com <khatami.maysam@gmail.com>
2023-07-02 14:19:48 +0330

    Aryashimi Update Footer Js

--- wp-content/themes/hdm/footer.php
+++ wp-content/themes/hdm/footer.php
@@ -1,4 +1,7 @@
-<!--<script type="text/javascript" defer src="--><?//=get_template_directory_uri()?><!--/assets/js/owl.carousel.min.js"></script>-->
+<script type="text/javascript" defer src="<?=get_template_directory_uri()?>/assets/js/jquery.3.6.1.js"></script>
+<script type="text/javascript" defer src="<?=get_template_directory_uri()?>/assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" defer src="<?=get_template_directory_uri()?>/assets/js/owl.carousel.min.js"></script>
+<script type="text/javascript" defer src="<?=get_template_directory_uri()?>/assets/js/script.js"></script>
 
 
 <?php wp_footer();?>
