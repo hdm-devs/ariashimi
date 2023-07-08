7c3cba7a367bbb67a127c5a22986e46e19251930
elhamhdmteam <hdmteam2328@gmail.com>
2023-07-08 09:37:04 +0330

    ariashimi edit google ads.php

--- wp-content/themes/hdm/Google-ads.php
+++ wp-content/themes/hdm/Google-ads.php
@@ -2,10 +2,10 @@
 
 // Template Name: Google Ads Page Fa
 
-get_header('home');
+get_header();
 ?>
     <section class="Home_Page">
         <?=the_content()?>
     </section>
 <?php
-get_footer('home');
+get_footer();
